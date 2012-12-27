c33d0ccb7f1010b9feccba8a7c2011d94e9df3e6
Damon Davison <damon@allolex.net>
2012-12-27 22:30:54 +0000

    WIP Creates Order resource specs without fixtures

---
+++ spec/api/order_service_resources/order_search_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Nacre::API::OrderSearch do
+  let(:connection) { mock("connection") }
+  let(:search_url) { 'order-service/order-search' }
+
+  before do
+    Nacre::API::OrderSearch.stub(:connection).and_return(connection)
+  end
+
+  context "when no search query is provided" do
+    before do
+      response = mock("response")
+      response.stub(:body).and_return(response_json)
+      connection.should_receive(:get).
+        with(search_url).
+        any_number_of_times.
+        and_return(response)
+    end
+
+    describe "#results" do
+      let(:response_json) { "{ foo : 'bar' }" }
+      let(:returned_results) { mock("a result") }
+
+      it "should return a search results object" do
+        Nacre::API::OrderSearchResults.should_receive(:new_from_json).
+          with(response_json).
+          and_return(returned_results)
+
+        results = Nacre::API::OrderSearch.new(search_url).results
+        results.should == returned_results
+      end
+    end
+  end
+
+
+  describe "when search params are provided" do
+    let(:param_list) {
+      []
+    }
+  end
+end
---
+++ spec/api/order_service_resources/order_spec.rb
@@ -0,0 +1,124 @@
+require 'spec_helper'
+
+describe Nacre::API::Order do
+  let(:connection) { mock("connection") }
+
+  before do
+    Nacre::API::Order.stub(:connection).and_return(connection)
+  end
+
+  context "when instantiated with no data" do
+    let(:model) {
+      Nacre::API::Order.new
+    }
+
+    it 'should create an instance' do
+      model.should be_a(Nacre::API::Order)
+    end
+  end
+
+  context "when instantiated with valid data" do
+    let(:valid_params) {}
+
+    let(:model) {
+      Nacre::API::Order.new(valid_params)
+    }
+
+    describe "initialization" do
+      it 'should create an instance' do
+        model.should be_a(Nacre::API::Order)
+      end
+
+      it 'should have a list of fields' do
+        model.class.fields.should == [
+          :id, :parent_order_id, :order_type_code,
+          :reference, :acknowledged, :order_status,
+          :stock_status_code, :allocation_status_code,
+          :placed_on, :created_on, :created_by_id,
+          :price_list_id, :price_mode_code, :delivery,
+          :invoices, :currency, :total_value,
+          :assignment, :parties, :order_rows
+        ]
+      end
+
+      pending 'should contain the correct model data' do
+        model.id.should == 1
+        model.order_type_code.should == ''
+      end
+    end
+
+    describe "find" do
+      before do
+        response = mock("response")
+        response.stub(:body).and_return(response_json)
+        connection.should_receive(:get).
+          with("/order-service/order/1").
+          any_number_of_times.
+          and_return(response)
+      end
+
+      context "when an empty list comes back" do
+        let(:response_json) { { "response" => [] }.to_json }
+
+        it 'should return nil' do
+          Nacre::API::Order.find(1008).should be_nil
+        end
+      end
+
+      context "when one valid value comes back" do
+        let(:results) { [] }
+        let(:response_json) { IO.read("spec/fixtures/json/order.json") }
+
+        it 'should return a valid Order' do
+          order = Nacre::API::Order.find(1008)
+          order.should be_a(Nacre::API::Order)
+          order.id.should == 1008
+          order.identity["sku"].should == "SKU0001"
+          #order.orderTypeId.should == 1  #TODO uncamelize this
+        end
+      end
+    end
+
+    describe "all" do
+      context "when an empty list comes back" do
+        it 'should return an empty array' do
+          Nacre::API::OrderSearch.any_instance.should_receive(:results).and_return(Nacre::API::OrderSearchResults.new)
+          all_orders = Nacre::API::Order.all
+          all_orders.should be_a(Array)
+          all_orders.should be_empty
+        end
+      end
+
+      context "when a list with actual values comes back" do
+        let(:response_json) { IO.read("spec/fixtures/json/order.json") }
+
+        it 'should return an array of Orders' do
+          search_results = Nacre::API::OrderSearchResults.new_from_json(
+              IO.read("spec/fixtures/json/order_search_result.json")
+          )
+          Nacre::API::OrderSearch.any_instance.should_receive(:results).and_return(search_results)
+
+          response = mock("response")
+          response.stub(:body).and_return(response_json)
+          connection.should_receive(:get).
+            with("/order-service/order/1000,1001,1002,1003,1004,1005,1006").
+            and_return(response)
+
+          orders = Nacre::API::Order.all
+          orders.should be_a(Array)
+          orders.length.should == 1
+
+          order = orders.first
+          order.should be_a(Nacre::API::Order)
+          order.id.should == 1008
+          order.identity["sku"].should == "SKU0001"
+          #order.orderTypeId.should == 1  #TODO uncamelize this
+        end
+      end
+    end
+
+    describe "save"
+
+    describe "delete"
+  end
+end
